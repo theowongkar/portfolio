e5bb2df74569838328e30ae0c2497b6354fd9c80
Theoterra Pitray Wongkar <theowongkar.tech@gmail.com>
2025-10-01 22:07:55 +0800

    Install Additional Dependencies and Add Assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import Home from "./pages/home";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
   return (
     <>
-      <Home />
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Home />
+      </ThemeProvider>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,5 @@ export default defineConfig({
       "@": path.resolve("./src"),
     },
   },
+  assetsInclude: ["**/*.glb"],
 });
